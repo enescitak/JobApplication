18abedf0bc77870b9d0ccb54b980c19cb33efeeb
enescitak <imposing-@hotmail.com>
2023-08-22 21:19:53 +0300

    Added new header tag.

---
+++ JobApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JobApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
